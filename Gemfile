4b6057885dfc959565ee19d5403e6943c276b9e7
scottmotte <scott@scottmotte.com>
2012-01-13 01:14:26 -0800

    initial

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :rubygems
+
+gem 'sinatra'
+gem 'thin'
+
+group :development do
+  gem 'foreman'
+  gem 'heroku'
+end
---
+++ app.rb
@@ -0,0 +1,3 @@
+get '/' do
+  "Sinatra Heroku Cedar Template - The bare minimum for a sinatra app on cedar, running thin, and using bundler."
+end
